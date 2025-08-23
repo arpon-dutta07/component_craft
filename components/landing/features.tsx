36312642620bdaa9951ce28394a22ecc2ed17cae
Arpon Dutta <arpongen3094@gmail.com>
2025-08-23 14:24:50 +0530

    #

---
+++ components/landing/features.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react'
+import  { motion } from "motion/react"
+import  image from "next/image";
+
+
+
+const Features = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Features
