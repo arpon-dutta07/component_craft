4a9692144c298da2ac275ba6d6d6c54c0de7a7c2
Arpon Dutta <arpongen3094@gmail.com>
2025-08-25 23:30:54 +0530

    #

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,40 @@ const withMDX = createMDX();
 
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'ferf1mheo22r9ira.public.blob.vercel-storage.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    outputFileTracingIncludes: {
+        "/**": ["components/componentcraftui/**/*"],
+    },
+    async headers() {
+        return [
+            {
+                source: "/r/:path*",
+                headers: [
+                    {
+                        key: "Cache-Control",
+                        value: "public, max-age=31536000, immutable",
+                    },
+                ],
+            },
+        ];
+    },
+
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'ferf1mheo22r9ira.public.blob.vercel-storage.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default withMDX(nextConfig);
